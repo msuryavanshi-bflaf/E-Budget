b6f48a07c508afe74e0cd0463681204f0e8523c3
MAYURI SURYAVANSHI <msuryavanshi@bflaf.com>
2023-02-28 17:43:51 +0530

    azure login page component

--- E-Budget/src/app/app.module.ts
+++ E-Budget/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AzureLoginComponent } from './components/azure-login/azure-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AzureLoginComponent
   ],
   imports: [
     BrowserModule,
