199ca59ddad72927b4a3f5b7a377dfa2685476b9
priyankalalage98 <plalage@bflaf.com>
2023-03-01 11:23:50 +0530

    landing page

--- E-Budget/src/app/app.module.ts
+++ E-Budget/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AzureLoginComponent } from './components/azure-login/azure-login.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AzureLoginComponent
+    AzureLoginComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
