90cf446caaeecb9ff4a7f3d0997bfe103f6e18a7
MAYURI SURYAVANSHI <msuryavanshi@BFLAF.COM>
2023-03-01 14:39:40 +0530

    remove files

--- E-Budget/src/app/app.module.ts
+++ E-Budget/src/app/app.module.ts
@@ -5,13 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AzureLoginComponent } from './components/azure-login/azure-login.component';
-import { AzureLoginTestingComponent } from './azure-login-testing/azure-login-testing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AzureLoginComponent,
-    AzureLoginTestingComponent
+    AzureLoginComponent
   ],
   imports: [
     BrowserModule,
