045baa50aef5bd070fdd5e7df52b9b3911acadd9
MAYURI SURYAVANSHI <msuryavanshi@BFLAF.COM>
2023-03-01 12:32:24 +0530

    testing

---
+++ E-Budget/src/app/testing/azure-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-azure-login',
+  templateUrl: './azure-login.component.html',
+  styleUrls: ['./azure-login.component.scss']
+})
+export class AzureLoginComponent {
+
+}
