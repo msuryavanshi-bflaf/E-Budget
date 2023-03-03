3169b33735b910bd26440333050d61aff6ed9ca8
MAYURI SURYAVANSHI <msuryavanshi@bflaf.com>
2023-03-03 15:20:25 +0530

    merge changes

--- E-Budget/src/app/app.module.ts
+++ E-Budget/src/app/app.module.ts
@@ -4,18 +4,14 @@ import { MaterialModule } from './shared/material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AzureLoginComponent } from './components/azure-login/azure-login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AzureLoginComponent,
     HeaderComponent,
     LandingPageComponent
-
-    
   ],
   imports: [
     BrowserModule,
