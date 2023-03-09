6de0d19dc9f14faec511edc3875c5f92ab535ea3
priyankalalage98 <plalage@bflaf.com>
2023-03-09 09:57:11 +0530

    Generate Po completed

--- E-Budget/src/app/components/po-generate/po-generate.component.ts
+++ E-Budget/src/app/components/po-generate/po-generate.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+import { AppConstant } from 'src/app/constants/app.constants';
+
 @Component({
   selector: 'app-po-generate',
   templateUrl: './po-generate.component.html',
   styleUrls: ['./po-generate.component.scss']
 })
 export class PoGenerateComponent {
+
+  public generatePOForm !: FormGroup;
+
+  constructor(private router: Router, private fb: FormBuilder) { }
+
+
+  ngOnInit() {
+
+    this.initGeneratePOForm();
+  }
+
+
+  initGeneratePOForm() {
+
+    this.generatePOForm = this.fb.group({
+
+      'poNumber': ['', [Validators.minLength(4)]],
+
+      'budgetCode': ['', [Validators.minLength(4)]],
+
+      'poStatus': ['', Validators.required],
+
+      'vendorName': ['', [Validators.minLength(4), Validators.maxLength(15)]],
+
+      'createdBy': ['', [Validators.minLength(4)]],
+
+      'poAmount': [''],
+
+      'modifyBy': ['', [Validators.minLength(4)]],
+
+      'modifyDate': [''],
+
+      'remark': [''],
+
+      'crDescription':['',[Validators.minLength(5)]],
+
+      'poExpiryDate':['',[Validators.required]]
+
+    });
+
+  }
+
+  generatePO() {
+
+    this.router.navigate([`/${AppConstant.GENERATEINVOICE}`])
+
+  }
+
 
 }
