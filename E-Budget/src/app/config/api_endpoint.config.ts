1c77fafd8ec0efb88c81889324185f7659c92ba4
priyankalalage98 <plalage@bflaf.com>
2023-03-31 16:50:16 +0530

    add new routings changes

--- E-Budget/src/app/config/api_endpoint.config.ts
+++ E-Budget/src/app/config/api_endpoint.config.ts
@@ -1,8 +1,18 @@
 export const API_END_POINTS = {
-    VENDOR: '/vendor',
-    BUDGET_CATEGORY:'/budgetCategory',
-    SUBCATEGORY:'/subCategory',
-    BUDGETCATEGORYLIST:'/findAllBudgetCategory',
+    // VENDOR: '/vendor',
+    // BUDGET_CATEGORY:'/budgetCategory',
+    // SUBCATEGORY:'/subCategory',
+    // BUDGETCATEGORYLIST:'/findAllBudgetCategory',
+    // CREATEBUDGET:'/createbudget',
+    // VENDORNAMELIST:'/findAllVendor'
+    VENDOR: '/vendor/vendor',
+    BUDGET_CATEGORY:'/budgetCategory/budgetCategory',
+    SUBCATEGORY:'/subCategory/subCategory',
+    BUDGETCATEGORYLIST:'/budgetCategory/findAllBudgetCategory',
+    BUDGETSUBCATEGORYLIST:'/subCategory/findAllSubBudgetCategory',
     CREATEBUDGET:'/createbudget',
-    VENDORNAMELIST:'/findAllVendor'
+    VENDORNAMELIST:'/vendor/findAllVendor',
+    BUDGETCATEGORYDETAILS:'/budgetCategoryDetails',
+    DELETECATEGORY:'/deleteCategory',
+    BUDGETCODELIST:'/findAllCodeList'
 }
