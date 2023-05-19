243777c730675051501eefc9abda640d72d52b9d
priyankalalage98 <plalage@bflaf.com>
2023-05-19 12:25:38 +0530

    added some changes in api backend url and home component.

--- E-Budget/src/environments/environment.ts
+++ E-Budget/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     localhost: false,
     appName: "E-Budget",
-    javaEndPoint: "http://localhost:8085",
+    javaEndPoint: "http://localhost:8181",
     // javaEndPoint: "http://10.44.33.101:8085",
     production:false,
   
